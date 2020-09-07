5f47ac1fb7e0182c02cb3a1ad65fedf412a32585
Sasirekha Kambhampaty <ksasirekha@gmail.com>
2020-09-07 12:39:46 +0530

    Initial Commit

---
+++ ios-mvc-exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-mvc-exercise
+//
+//  Created by Sasirekha Kambhampaty on 9/7/20.
+//  Copyright © 2020 Sasirekha Kambhampaty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
